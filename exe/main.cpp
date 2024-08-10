b1609fb602513b61b58a465f06edde91342402f6
mastamysta <benjamin-read@hotmail.co.uk>
2024-08-10 12:16:06 +0100

    Remove some extraneous complexity to get a better representation of performance impact of indirect calls

--- exe/main.cpp
+++ exe/main.cpp
@@ -13,37 +13,21 @@ static auto direct(uint32_t a, uint32_t b) -> uint32_t
 #endif
 
 
-template <typename T, std::size_t S>
-class g
-{
-public:
-    constexpr auto operator ()() -> std::array<T, S>
-    {
-        std::array<T, S> ret = { 0 };
-
-        for (int i = 0; i < S; i++)
-        {
-            ret[i] = (i * 3) + 9;
-        }
-
-        return ret;
-    }
-};
-
 #define LIST_SIZE 100000
 
 int main()
 {
-    g<uint32_t, LIST_SIZE> gen;
-    auto data = gen();
 
-    for (int i = 0; i < LIST_SIZE; i++)
+    for (int j = 0; j < 10000; j++)
     {
-#ifdef INDIRECT
-    indirect(data[i], data[LIST_SIZE - i]);
-#else
-    direct(data[i], data[LIST_SIZE - i]);
-#endif   
+        for (int i = 0; i < LIST_SIZE; i++)
+        {
+    #ifdef INDIRECT
+        indirect(i, LIST_SIZE - i);
+    #else
+        direct(i, LIST_SIZE - i);
+    #endif   
+        }
     }
 
     return 0;
